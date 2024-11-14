ed8123a7ba6ebb930d59c5fc2c1fcc5052b147ee
Damian Ramirez <damian.ramirez@lambdaclass.com>
2024-11-14 20:27:19 -0300

    Basic implementation to update the cells with the new state and count the neighbors

--- src/conways.rs
+++ src/conways.rs
@@ -1,20 +1,80 @@
-const COLUMNS: usize = 40;
-const ROW: usize = 40;
+const COLUMNS: usize = 32;
+const ROWS: usize = 32;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub enum CellState {
-  Alive,
-  Dead
+    Alive,
+    Dead,
 }
 
+#[derive(Clone, Copy)]
+pub struct Position(pub usize, pub usize);
+
 pub struct Conways {
-  pub grid: Vec<Vec<CellState>>
+    pub grid: Vec<Vec<CellState>>,
 }
 
 impl Conways {
-    pub fn new() -> Self {
-      Self {
-        grid: vec![vec![CellState::Dead; ROW]; COLUMNS]
-      }
+
+    pub fn from(alive_cells: Vec<Position>) -> Self {
+        let mut grid = vec![vec![CellState::Dead; COLUMNS]; ROWS];
+        
+        for Position(x, y) in alive_cells {
+            if x < ROWS && y < COLUMNS {
+                grid[x][y] = CellState::Alive;
+            }
+        }
+        
+        Self { grid }
+    }
+
+    pub fn update_cells(&mut self) {
+        let mut new_grid = self.grid.clone();
+
+        // Iterate over the grid, updating with the new state
+        for x in 0..self.grid.len() {
+            for y in 0..self.grid.len() {
+                let neighbors = self.count_neighbors(Position(x, y));
+                let cell = self.grid[x][y];
+                match cell {
+                    CellState::Alive => {
+                        if neighbors > 3 || neighbors < 2 {
+                            new_grid[x][y] = CellState::Dead;
+                        }
+                    }
+                    CellState::Dead => {
+                        if neighbors == 3 {
+                            new_grid[x][y] = CellState::Alive;
+                        }
+                    }
+                }
+            }
+        }
+        
+        self.grid = new_grid;
+    }
+
+    pub fn count_neighbors(&self, position: Position) -> usize {
+        let mut count = 0;
+
+        // Position of the eight neighbors
+        let directions = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),           (0, 1),
+            (1, -1),  (1, 0),   (1, 1),
+        ];
+
+        for (x, y) in directions.iter() {
+            let new_x = position.0 as isize + x;
+            let new_y = position.1 as isize + y;
+
+            // Avoid leaving the grid
+            if new_x >= 0 && new_x < ROWS as isize && new_y >= 0 && new_y < COLUMNS as isize {
+                if let CellState::Alive = self.grid[new_x as usize][new_y as usize] {
+                    count += 1;
+                }
+            }
+        }
+        count
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use macroquad::prelude::*;
 use conways::Conways;
 
 const SIZE: f32 = 16.;
+const UPDATE_TIMER: f64 = 0.1;
 
 fn conf() -> Conf {
     Conf {
@@ -18,10 +19,33 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
+
+    let mut last_updated = 0_f64;
+
+    let alive_cells = vec![
+        conways::Position(1, 3),
+        conways::Position(1, 5),
+        conways::Position(2, 1),
+        conways::Position(2, 7),
+        conways::Position(3, 3),
+        conways::Position(3, 6),
+        conways::Position(4, 2),
+        conways::Position(4, 4),
+        conways::Position(4, 8),
+        conways::Position(5, 1),
+        conways::Position(5, 5),
+        conways::Position(5, 7),
+        conways::Position(6, 3),
+        conways::Position(6, 4),
+        conways::Position(6, 8),
+        conways::Position(7, 2),
+        conways::Position(7, 5),
+    ];
+    let mut conways: Conways = Conways::from(alive_cells);
+
     loop {
         clear_background(BLACK);
 
-        let conways: Conways = Conways::new();
         for (x_row, row) in conways.grid.iter().enumerate() {
             for (y_col, col) in row.iter().enumerate() {
                 draw_rectangle(
@@ -32,8 +56,14 @@ async fn main() {
                     match col {
                         conways::CellState::Alive => WHITE,
                         conways::CellState::Dead => BLACK,
-                    },);
+                    }
+                );
             }
+        }
+
+        if get_time() - last_updated > UPDATE_TIMER {
+            last_updated = get_time();
+            conways.update_cells();
         }
 
         next_frame().await
